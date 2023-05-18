c9b85018fb692b69d7f32db4aeee2cbe4124e9f5
Ulises Ruiz <supertepo@outlook.com>
2023-05-18 17:30:14 -0600

    clase01Terminada

---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,12 @@
+import "./CampoTexto.css"
+
+const CampoTexto = (props) => {
+    console.log("Datos: ", props.titulo)
+    const placeholderModificador = `${props.placeholder}...`
+    return <div className="campo-texto">
+        <label>{props.titulo}</label>
+        <input placeholder={placeholderModificador} />
+    </div>
+}
+
+export default CampoTexto
