bd6925af6ed658bb32bfd0783933b74f09e04e77
Ulises Ruiz <supertepo@outlook.com>
2023-05-22 19:13:31 -0600

    Clase07 Terminada

--- src/componentes/Equipo/index.js
+++ src/componentes/Equipo/index.js
@@ -1,17 +1,27 @@
 import "./Equipo.css";
 import Colaborador from "../Colaborador";
+import hexToRgba from 'hex-to-rgba';
+
 const Equipo = (props) => {
   //Destruccturacion
-  const { colorPrimario, colorSecundario, titulo } = props.datos;
-  const { colaboradores, eliminarColaborador } = props;
+  const { colorPrimario, colorSecundario, titulo, id } = props.datos;
+  const { colaboradores, eliminarColaborador, actualizarColor } = props;
   const obj = {
-    backgroundColor: colorSecundario,
+    backgroundColor: hexToRgba(colorPrimario,0.6),
   };
   const estiloTitulo = { borderColor: colorPrimario };
   return (
     <>
       {colaboradores.length > 0 && (
         <section className="equipo" style={obj}>
+          <input 
+          type='color'
+          className="input-color"
+          value={colorPrimario}
+          onChange={(evento) => {
+            actualizarColor(evento.target.value, id)
+          }}
+          />
           <h3 style={estiloTitulo}>{titulo}</h3>
           <div className="colaboradores">
             {colaboradores.map((colaborador, index) => (
